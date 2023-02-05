af63c87e46dea4a0d091b7d312b58b5963a1d6a4
Jake Kwak <jakekwak@gmail.com>
2023-02-05 13:08:05 +0900

    Update svelte.config.js
    Docker일 때 CORS 문제가 생김

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,26 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('vite').Plugin} */
+const viteServerConfig = {
+    name: 'log-request-middleware',
+    configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET");
+            res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+            res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+            next();
+        });
+    }
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ edge: true })
+		adapter: adapter({ edge: true }),
+		vite: {
+            		plugins: [viteServerConfig]
+        	}
 	},
 
 	vitePlugin: {
